8385a75ecf461c1afa5a04024d12cac588f5c5c4
Michael Goldsmith <magoldsm>
2021-02-13 13:51:33 -0800

    Minor changes

--- json_test.py
+++ json_test.py
@@ -3,7 +3,7 @@ import json
 objects = []
 
 for i in range(10):
-    object = { "objectType":"PowerCell", "x":i*1.1, "y":i*11.0, "z":i*110.0 }
+    object = { "objectLabel":"PowerCell", "x":i*1.1, "y":i*11.0, "z":i*110.0, "confidence":52 }
     objects.append(object)
 
 jsonObjects = json.dumps(objects)
