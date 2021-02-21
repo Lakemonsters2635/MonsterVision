74c7ca59d899e51ac4a3d7cae114d32df46bc655
magoldsm <magoldsm@winstocksw.com>
2021-02-20 22:14:37 -0800

    Demoing JSON serialization with sig figs.

--- json_test.py
+++ json_test.py
@@ -1,11 +1,15 @@
 import json
 
+class PrettyFloat(float):
+    def get(self):
+        return round(self, 1)
+
 objects = []
 
 for i in range(10):
-    object = { "objectLabel":"PowerCell", "x":i*1.1, "y":i*11.0, "z":i*110.0, "confidence":52 }
+    object = { "objectLabel":"PowerCell", "x":round(i*1.1, 1), "y":round(i*11.0, 1), "z":round(i*110.0, 1), "confidence":52 }
     objects.append(object)
 
-jsonObjects = json.dumps(objects)
+jsonObjects = json.dumps(objects, indent=4)
 
 print(jsonObjects)
